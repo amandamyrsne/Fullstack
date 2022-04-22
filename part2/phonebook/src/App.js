cb705e4321e2d2de39cdf00e8e2dab308f1b448e
Amanda Myrevik-Snellman <amanda.myrevik-snellman@centria.fi>
2022-04-22 10:53:23 +0300

    Exercise 2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,57 +6,66 @@ import PersonList from './Components/PersonList'
 import personsService from './services/persons'
 
 const App = () => {
-  
+
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [name, setName] = useState('')
-  const [foundUsers, setFoundUsers] = useState(persons)
 
   useEffect(() => {
     axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      console.log('promise fulfilled')
-      setPersons(response.data)
-    })
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
   }, [])
 
   const addName = (event) => {
-    event.preventDefault()    
+    event.preventDefault()
     const noteObject = {
       name: newName,
       number: newNumber,
     }
-    personsService
-      .create(noteObject)
-      .then(returnedNote => {
-        setPersons(foundUsers.concat(returnedNote))
-        setNewName('')
-        setNewNumber('')
-      })
-  
-      var same = "";
-      persons.forEach(name => {
-        
-        if (JSON.stringify(name) === JSON.stringify(noteObject)){
-          window.alert(newName + ' is already added to phonebook');
-          same = "true";
-        }})
 
-        if( same === "true"){
-           return;
+
+    let same = false;
+    persons.forEach(name => {
+      console.log(name.name, noteObject.name)
+      if (JSON.stringify(name.name) === JSON.stringify(noteObject.name)) {
+        if (window.confirm(newName + ' is already added to phonebook, replace the old number with a new one?')){
+            personsService
+            .update(name.id, noteObject)
+            .then(returnedPerson => {
+              setPersons(persons.map(person => person.id !== name.id ? person : returnedPerson))
+          })
         }
-        else{
-          setPersons(persons.concat(noteObject))
+
+        same = true;
+      }
+    })
+    console.log(same)
+    if (same === true) {
+      setNewName('')
+      setNewNumber('')
+      return;
+    }
+    else {
+
+      personsService
+        .create(noteObject)
+        .then(returnedNote => {
+          setPersons(persons.concat(returnedNote))
           setNewName('')
           setNewNumber('')
-        }}
+        })
+    }
+  }
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-  const handleNumberChange = (event) =>{
+  const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
@@ -68,34 +77,35 @@ const App = () => {
         return person.name.toLowerCase().startsWith(word.toLowerCase())
       })
       setPersons(results)
-    } 
+    }
     else {
       setPersons(persons)
     }
     setName(word)
   })
   const deleteAccount = (id) => {
-    
+
     console.log(persons, id)
 
     const personname = persons.find(person => person.id === id)
     console.log('person name', personname)
-    if(window.confirm(`Delete ${personname.name}?`)){
+    if (window.confirm(`Delete ${personname.name}?`)) {
       personsService
-      .remove(id)// 
-      .then(response => {
+        .remove(id)
+        .then(response => {
           setPersons(persons.filter(person => person.id !== id))
-         
-      })
-    }}
+
+        })
+    }
+  }
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter name={name} filter={filter} />
       <h2>Add a new</h2>
-      <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+      <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <PersonList persons={persons} deleteAccount = {deleteAccount}/>
+      <PersonList persons={persons} deleteAccount={deleteAccount} />
     </div>
   )
 }
