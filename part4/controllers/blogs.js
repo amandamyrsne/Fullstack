100bd49a3273c71eb3a63d54e063ba82493dea9e
amandamyrsne <92168679+amandamyrsne@users.noreply.github.com>
2022-05-04 10:54:36 +0300

    Started on part4

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,25 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blogs')
+
+blogsRouter.get('/', async (request, response) => {
+  const blog = await Blog.find({})
+  response.json(blog)
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch(exception) {
+    next(exception)
+  }
+})
+module.exports = blogsRouter
